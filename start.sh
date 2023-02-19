6b286d000fae6812867f2abf4bba14855dd118d4
vv12x <120430865+vv12x@users.noreply.github.com>
2023-02-18 19:26:45 -0500

    a

--- start.sh
+++ start.sh
@@ -1,15 +1,5 @@
 export PUPPETEER_EXECUTABLE_PATH=$(which chromium)
 cd js
-npm install axios@1.2.1
-npm install fakebrowser@0.0.66
-npm install fast-email-validator@1.0.0
-npm install https-proxy-agent@5.0.1
-npm install node-fetch@2.0.0
-npm install puppeteer@13.7.0 --save
-npm install puppeteer-extra@3.2.3
-npm install puppeteer-extra-plugin@3.2.0
-npm install puppeteer-extra-plugin-adblocker@2.12.0
-npm install puppeteer-extra-plugin-user-preferences@2.4.0
-npm install zy-kit@1.0.0
+npm install axios@1.2.1 fakebrowser@0.0.66 fast-email-validator@1.0.0 https-proxy-agent@5.0.1 node-fetch@2.0.0 puppeteer-extra@3.2.3 puppeteer-extra-plugin@3.2.0 puppeteer-extra-plugin-adblocker@2.12.0 puppeteer-extra-plugin-user-preferences@2.4.0 npm install zy-kit@1.0.0 --save
 which chromium
 node index.js
