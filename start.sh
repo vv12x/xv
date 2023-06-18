8276304294c57865fa6bdd7ce3bddb7443b081a5
vv12x <120430865+vv12x@users.noreply.github.com>
2023-06-18 17:57:38 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export PUPPETEER_EXECUTABLE_PATH=$(which chromium)
 cd js
-npm install @extra/proxy-router@3.1.6 axios@1.2.1 fakebrowser@0.0.66 fast-email-validator@1.0.0 https-proxy-agent@5.0.1 node-fetch@2.0.0 puppeteer-extra@3.2.3 puppeteer-extra-plugin@3.2.0 puppeteer-extra-plugin-adblocker@2.12.0 puppeteer-extra-plugin-user-preferences@2.4.0 npm install zy-kit@1.0.0 --save
+npm install @extra/proxy-router@3.1.6 puppeteer-extra-plugin-timezone@1.0.5 axios@1.2.1 fakebrowser@0.0.66 fast-email-validator@1.0.0 https-proxy-agent@5.0.1 node-fetch@2.0.0 puppeteer-extra@3.2.3 puppeteer-extra-plugin@3.2.0 puppeteer-extra-plugin-adblocker@2.12.0 puppeteer-extra-plugin-user-preferences@2.4.0 npm install zy-kit@1.0.0 --save
 which chromium
 node index.js
